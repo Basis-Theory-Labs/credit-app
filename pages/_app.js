be67a1b193a408aa7d9d763fa8cc8bb156893b52
James Armstead <james@basistheory.com>
2022-05-17 21:28:21 -0500

    fiixing build error

--- pages/_app.js
+++ pages/_app.js
@@ -15,9 +15,6 @@ export default function MyApp(props) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content={theme.palette.primary.main} />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </Head>
 
       <ThemeProvider theme={theme}>
